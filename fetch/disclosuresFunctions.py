0f29bf52fd16a4c0de79b3d10bb5d0d72d8eb9f2
Aayla Pozho <aayla@aayla.dev>
2020-04-16 01:58:32 -0600

    Added Senate Financial Disclosures and stock performance measurement

---
+++ fetch/disclosuresFunctions.py
@@ -0,0 +1,15 @@
+import requests
+
+
+def getDisclosures(url):
+    """
+    Get disclosures from senatestockwatcher.com
+    :param url:
+    :return: Array
+    """
+    data = requests.get(url).json()
+    return data
+
+
+def getTickerValue(url, symbol, key):
+    pass
